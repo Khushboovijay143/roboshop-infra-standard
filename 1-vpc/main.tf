a12924c7b2578113f3088bfc67628d20aef8bfa2
Khushboovijay143 <vijay1991080619830514@gmail.com>
2024-09-20 13:16:41 +0530

    updated

--- 1-vpc/main.tf
+++ 1-vpc/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "git::https://github.com/Khushboovijay143/terraform/tree/main/terraform-module-vpc-advanced.git"
+  source = "git::https://github.com/Khushboovijay143/terraform/terraform-module-vpc-advanced.git"
   project_name = var.project_name #"roboshop" instead of hard coding we are keeping in variables.tf
   cidr_block = var.cidr_block
   common_tags = var.common_tags
